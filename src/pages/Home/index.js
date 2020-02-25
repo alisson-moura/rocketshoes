6398032e146f38ac093c7debb9d2a4eab5d82900
Alisson Matheus de Oliveira Moura <alisson.mo.moura@outlook.com.br>
2020-02-25 10:42:42 -0300

    Buscando dados da API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,66 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { ProductList } from './styles';
 
-export default function Home() {
-    return (
-        <ProductList>
-            <li>
-                <img
-                    src="https://images.lojanike.com.br/500x500/produto/99904_783137_20190704203531.png"
-                    alt="Jumpman"
-                />
-                <strong>Air Jordan Jumpman</strong>
-                <span>$600,00</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" /> 3
-                    </div>
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-            <li>
-                <img
-                    src="https://images.lojanike.com.br/500x500/produto/195056_1802367_20191114122851.jpg"
-                    alt="Jumpman"
-                />
-                <strong>Air Jordan 1 Mid</strong>
-                <span>$600,00</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" /> 3
-                    </div>
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-            <li>
-                <img
-                    src="https://images.lojanike.com.br/500x500/produto/180596_1772586_20191029224327.png"
-                    alt="Jumpman"
-                />
-                <strong>Jordan React Havoc</strong>
-                <span>$600,00</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" /> 3
-                    </div>
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-            <li>
-                <img
-                    src="https://images.lojanike.com.br/500x500/produto/193775_1849606_20191126114050.jpg"
-                    alt="Jumpman"
-                />
-                <strong>Air Jordan 1 Jester</strong>
-                <span>$600,00</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" /> 3
-                    </div>
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-        </ProductList>
-    );
+import api from '../../services/api';
+import { formatPrice } from '../../util/format';
+
+export default class Home extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            products: [],
+        };
+    }
+
+    async componentDidMount() {
+        const response = await api.get('/products');
+        const data = response.data.map(product => ({
+            ...product,
+            priceFormatted: formatPrice(product.price),
+        }));
+        this.setState({ products: data });
+    }
+
+    render() {
+        const { products } = this.state;
+        return (
+            <ProductList>
+                {products.map(product => (
+                    <li key={product.id}>
+                        <img src={product.image} alt={product.title} />
+                        <strong>{product.title}</strong>
+                        <span>{product.priceFormatted}</span>
+
+                        <button type="button">
+                            <div>
+                                <MdAddShoppingCart size={16} color="#FFF" /> 3
+                            </div>
+                            <span>ADICIONAR AO CARRINHO</span>
+                        </button>
+                    </li>
+                ))}
+            </ProductList>
+        );
+    }
 }
