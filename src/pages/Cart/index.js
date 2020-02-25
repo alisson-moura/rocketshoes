2e460df60f3b7d4003e5e94c4382d8041327cca5
Alisson Matheus de Oliveira Moura <alisson.mo.moura@outlook.com.br>
2020-02-25 10:15:00 -0300

    Estilização do carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,5 +1,73 @@
 import React from 'react';
+import {
+    MdRemoveCircleOutline,
+    MdAddCircleOutline,
+    MdDelete,
+} from 'react-icons/md';
+
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-    return <div />;
+    return (
+        <Container>
+            <ProductTable>
+                <thead>
+                    <tr>
+                        <th />
+                        <th>Produto</th>
+                        <th>Quantidade</th>
+                        <th>Sub-Total</th>
+                        <th />
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <img
+                                src="https://images.lojanike.com.br/500x500/produto/193775_1849606_20191126114050.jpg"
+                                alt="Jumpman"
+                            />
+                        </td>
+                        <td>
+                            <strong>Air Jordan 1 Jester</strong>
+                            <span>$600,00</span>
+                        </td>
+                        <td>
+                            <div>
+                                <button type="button">
+                                    <MdRemoveCircleOutline
+                                        size={20}
+                                        color="#7159c1"
+                                    />
+                                </button>
+                                <input type="number" readOnly value={2} />
+                                <button type="button">
+                                    <MdAddCircleOutline
+                                        size={20}
+                                        color="#7159c1"
+                                    />
+                                </button>
+                            </div>
+                        </td>
+                        <td>
+                            <strong>R$ 258,80</strong>
+                        </td>
+                        <td>
+                            <button type="button">
+                                <MdDelete size={20} color="#7159c1" />
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </ProductTable>
+
+            <footer>
+                <button type="button">Finalizar pedido.</button>
+                <Total>
+                    <span>Total</span>
+                    <strong>R$ 1920,28</strong>
+                </Total>
+            </footer>
+        </Container>
+    );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,66 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { ProductList } from './styles';
 
 export default function Home() {
-    return <div />;
+    return (
+        <ProductList>
+            <li>
+                <img
+                    src="https://images.lojanike.com.br/500x500/produto/99904_783137_20190704203531.png"
+                    alt="Jumpman"
+                />
+                <strong>Air Jordan Jumpman</strong>
+                <span>$600,00</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://images.lojanike.com.br/500x500/produto/195056_1802367_20191114122851.jpg"
+                    alt="Jumpman"
+                />
+                <strong>Air Jordan 1 Mid</strong>
+                <span>$600,00</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://images.lojanike.com.br/500x500/produto/180596_1772586_20191029224327.png"
+                    alt="Jumpman"
+                />
+                <strong>Jordan React Havoc</strong>
+                <span>$600,00</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://images.lojanike.com.br/500x500/produto/193775_1849606_20191126114050.jpg"
+                    alt="Jumpman"
+                />
+                <strong>Air Jordan 1 Jester</strong>
+                <span>$600,00</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+        </ProductList>
+    );
 }
